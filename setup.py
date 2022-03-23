1735ea4376694c2179ac62ce7d100a10b26f2558
Brandon Graham-Knight <69595580+brandongk-ubco@users.noreply.github.com>
2022-03-23 09:34:40 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     url="https://github.com/albumentations-team/autoalbument",
     install_requires=[
         "albumentations>=0.5.1",
-        "pytorch-lightning>=1.1.8,<1.2",
+        "pytorch-lightning",
         "torch>=1.6.0",
         "hydra-core>=1.0",
         "segmentation-models-pytorch>=0.2.1",
